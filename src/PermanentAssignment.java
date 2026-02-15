193ffcfbb1eba4e1f33d0e4db0e38c0c30703143
Larina Vasilina <larina.vasilina@list.ru>
2026-02-15 14:43:34 +0700

    Create class PermanentAssignment

---
+++ src/PermanentAssignment.java
@@ -0,0 +1,26 @@
+public class PermanentAssignment extends AbstractRoleAssignment {
+    private boolean revoked;
+
+    public PermanentAssignment(User user, Role role, AssignmentMetadata metadata) {
+        super(user, role, metadata);
+        this.revoked = false;
+    }
+
+    @Override
+    public boolean isActive() {
+        return !revoked;
+    }
+
+    @Override
+    public String assignmentType() {
+        return "PERMANENT";
+    }
+
+    public void revoke() {
+        this.revoked = true;
+    }
+
+    public boolean isRevoked() {
+        return revoked;
+    }
+}
